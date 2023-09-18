3948ed161f083071a77d9fad436ac915eff88ec0
mole99 <leomoser99@gmail.com>
2023-09-18 17:09:32 +0200

    Fix output name

--- src/tt_um_top_mole99.sv
+++ src/tt_um_top_mole99.sv
@@ -30,7 +30,7 @@ module tt_um_top_mole99 (
         .rrggbb     (uo_out[7:2]),
         .hsync      (uo_out[0]),
         .vsync      (uo_out[1]),
-        .next_line  (uio_out[0]),
+        .next_vertical  (uio_out[0]),
         .next_frame (uio_out[1])
     );
 
